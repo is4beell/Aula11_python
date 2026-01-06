0ebb00533d835ea5d9c3b4b3a6e2bdd24f8c7ced
is4beell <is4beell@gmail.com>
2026-01-06 19:45:44 -0300

    Implementa a bibioteca os

--- exemplo01.py
+++ exemplo01.py
@@ -1,6 +1,6 @@
 # Bibliotecas
 import random
-
+import os
 #  random.randint -> Números inteiros 
 n = random.randint(1, 10)
 m = random.randint(1, 10)
@@ -19,4 +19,6 @@ print(numero_decimal)
 #-------------------------------------------------------------------------------------
 # Gerar números aleatórios dentro de uma lista 
 lst_numeros = [random.randint(1, 10) for num in range(5)]
+os.system('cls')  #Limpa a tela antes de printar a lista 
+
 print(lst_numeros)
