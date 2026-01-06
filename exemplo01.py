2bfc8638f204dcd9cff01bf7786ef8dc4cfa493b
is4beell <is4beell@gmail.com>
2026-01-06 19:18:42 -0300

    Casas decimais

--- exemplo01.py
+++ exemplo01.py
@@ -4,4 +4,12 @@ import random
 #  random.randint -> Números inteiros 
 n = random.randint(1, 10)
 m = random.randint(1, 10)
-print(n,m)
+#print(n,m)
+
+
+# ------------------------------------------------------------------------------------
+# Gerar números decimais 
+n_decimal = random.uniform(1, 10)
+numero_decimal = round(n_decimal, 1) # Arredonda 
+print(f'{n_decimal: .2f}') # Formata para dias casas 
+print(numero_decimal)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+n_decimal = random.uniform(1, 10)
+print(f'{n_decimal: .2f}')
